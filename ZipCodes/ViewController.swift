6096a1b8ad63ee4f24d3e1180dd490bf20da2fa1
Vincent Cubit <vinniecubit@yahoo.com>
2022-03-06 20:42:46 -0600

    Initial Commit

---
+++ ZipCodes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZipCodes
+//
+//  Created by Vincent Cubit on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
